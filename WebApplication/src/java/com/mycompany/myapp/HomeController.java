5d6895d6f70c9cd2374fbc9f60b5b31291c2a197
Dahye Lee <melllamodahye@gmail.com>
2017-04-26 09:49:52 +0900

    4월26일 Block/Inline tag 코드 수정

--- WebApplication/src/java/com/mycompany/myapp/HomeController.java
+++ WebApplication/src/java/com/mycompany/myapp/HomeController.java
@@ -1,13 +1,29 @@
 package com.mycompany.myapp;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+
   @RequestMapping("/")
   public String home() {
+    System.out.println("home");
     return "home";
+  }
+
+  @RequestMapping("/html")
+  public String html() {
+    return "html";
+  }
+
+  @RequestMapping("/css/exam01")
+  public String cssExam01() {
+    return "css/exam01";
+  }
+  
+  @RequestMapping("/css/exam02")
+  public String cssExam02() { // 이 메소드 이름은 아무거나 상관없음. 잘 알아두자!이런게 내 코딩할 때 무쟈게 중요하게 작용한다...
+    return "css/exam02";
   }
 }
