81a6280043d28a25815fbcc0dbccee3902049722
Dahye Lee <melllamodahye@gmail.com>
2017-04-26 10:52:25 +0900

    4월26일 CSS 실습코드

--- WebApplication/src/java/com/mycompany/myapp/HomeController.java
+++ WebApplication/src/java/com/mycompany/myapp/HomeController.java
@@ -21,9 +21,19 @@ public class HomeController {
   public String cssExam01() {
     return "css/exam01";
   }
-  
+
   @RequestMapping("/css/exam02")
   public String cssExam02() { // 이 메소드 이름은 아무거나 상관없음. 잘 알아두자!이런게 내 코딩할 때 무쟈게 중요하게 작용한다...
     return "css/exam02";
+  }
+
+  @RequestMapping("/css/exam03")
+  public String cssExam03() { // 이 메소드 이름은 아무거나 상관없음. 잘 알아두자!이런게 내 코딩할 때 무쟈게 중요하게 작용한다...
+    return "css/exam03";
+  }
+
+  @RequestMapping("/javascript/exam01")
+  public String javascriptExam01() { // 이 메소드 이름은 아무거나 상관없음. 잘 알아두자!이런게 내 코딩할 때 무쟈게 중요하게 작용한다...
+    return "javascript/exam01";
   }
 }
