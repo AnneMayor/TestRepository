8b3264fa3cf5320db6a093fef01849c48b2234f6
Dahye Lee <melllamodahye@gmail.com>
2017-06-15 17:56:05 +0900

    6/15 Spring Template 작성

--- WebApplication1/src/test/java/com/mycompany/myapp/service/ServiceTest.java
+++ WebApplication1/src/test/java/com/mycompany/myapp/service/ServiceTest.java
@@ -4,16 +4,14 @@ import org.junit.Test;
 
 import com.mycompany.myapp.ApplicationContextLoader;
 
-public class ServiceTest extends ApplicationContextLoader{
-	
+public class ServiceTest extends ApplicationContextLoader {
 	@Test
 	public void writeTest() {
-		
-	}
+		//테스트 코드
+	}	
 	
 	@Test
 	public void getTest() {
-		
+		//테스트 코드
 	}
-	
 }
