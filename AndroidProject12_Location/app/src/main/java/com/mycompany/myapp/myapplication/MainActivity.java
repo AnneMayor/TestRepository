8734c87779db4535a3ba494667587631cab302c8
Dahye Lee <melllamodahye@gmail.com>
2017-07-07 17:53:42 +0900

    7/7 Android

--- AndroidProject12_Location/app/src/main/java/com/mycompany/myapp/myapplication/MainActivity.java
+++ AndroidProject12_Location/app/src/main/java/com/mycompany/myapp/myapplication/MainActivity.java
@@ -3,15 +3,23 @@ package com.mycompany.myapp.myapplication;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.pm.ActivityInfoCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import org.apache.commons.lang.StringEscapeUtils;
 import org.json.JSONArray;
@@ -19,6 +27,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -30,12 +40,16 @@ import okhttp3.Response;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private LocationManager locationManager;
+    private SupportMapFragment mapFragment;
+    private GoogleMap googleMap;
+    private List<LatLng> listLocation = new ArrayList<>();
+    private Polyline polyLine;
+    private LatLng currLatLng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         checkPermission();
     }
 
@@ -51,6 +65,8 @@ public class MainActivity extends AppCompatActivity {
         checkPermission();
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         // 10초 뒤에 위치측정을 다시한다는 의미. 거리에 대한 정보로 0미터로 설정해놓으면 거리와 상관없이 시간에 따라 위치정보를 받으라는 의미.
+        // 비동기 통신으로 이루어진다.
+        // 기지국으로 얻을 수 있는 정보가 소수임.
         locationManager.requestLocationUpdates(
                 LocationManager.NETWORK_PROVIDER,
                 10000,
@@ -60,14 +76,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private LocationListener locationListener = new LocationListener() {
+        // 아래 메소드가 callback 메소드
         @Override
         public void onLocationChanged(Location location) {
+            // location.getAltitude(): 이건 고도를 얻는 메소드.
+            // location.getBearing(): 휴대폰이 지구중심으로부터의 기울기 각도를 리턴. 이건 네트워크 제공자가 기지국일 경우엔 얻을 수 없음.
             double latitude = location.getLatitude();
             double longitude = location.getLongitude();
             Log.i(TAG, "위도: " + latitude);
             Log.i(TAG, "경도: " + longitude);
             // 아래 명령어 추가하면 위치정보를 딱 한 번만 받아오고 더이상 받아오지 않는다.
             locationManager.removeUpdates(locationListener);
+            showMap(latitude, longitude);
         }
 
         @Override
@@ -79,6 +99,49 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onProviderDisabled(String provider) {}
     };
+
+    private void showMap(double latitude, double longitude) {
+        currLatLng = new LatLng(latitude, longitude);
+
+        // 지도를 드로잉하기 위한 코드
+        // animateCamera vs moveCamera : animate는 현재 위치로 지도가 이동하는 모습까지 보이기. move는 현재 위치로 바로 움직이는 모습을 보여줌.
+        // 사용자가 확대해서 보기 편한 정도: 16 ~ 18
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currLatLng, 16));
+        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+        checkPermission();
+        googleMap.setMyLocationEnabled(true);
+
+        // 경로를 드로잉하기 위한 코드
+        listLocation.clear();
+        // 현재 위치 저장
+        listLocation.add(currLatLng);
+        // 지도를 클릭할 때마다 현재 위치 위도와 경도 listLocation에 저장
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                listLocation.add(latLng);
+                drawPath();
+    }
+
+    public void handleBtnMap() {
+        // fragment는 fragmentManager가 관리한다. 그렇기에 매니져를 먼저 얻고 시작해야함.
+        // fragment를 관리하는 주체는 FragmentManager! findbyID 이거 안통함...
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapFragment);
+        // 지도를 준비하기 위한 객체 초기화를 시키는 코드는 다음 아래와 같음.
+        // 구글 맵서비스로 네트워크로 연결하기 위한 비동기 통신이 이루어져야 함.
+        // callback: 비동기적으로 작업을 처리하고 작업이 완료되면 알려주는 방식. 언제 완료되었는지 알려주는 역할. 여러가지 callback방식이 있음.
+        // ex) ajax -> success: function(data)
+
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                // 요거 굉장히 유용한 코드임. 잘 알아두자.
+                MainActivity.this.googleMap = googleMap;
+                handleBtnCurrentLocation(null);
+            }
+        });
+    }
 
     public void handleBtnFromAddressToLocation(View v) {
         String url = "https://apis.daum.net/local/geo/addr2coord";
@@ -116,6 +179,33 @@ public class MainActivity extends AppCompatActivity {
             public void onFailure(Call call, IOException e) {
                 Log.i(TAG, e.toString());
             }
+        });
+    }
+
+    public void handleBtnCurrLocationReturn(View v) {
+        listLocation.add(currLatLng);
+        drawPath();
+    }
+
+    public void handleBtnPathRemove(View v) {
+        listLocation.clear();
+        listLocation.add(currLatLng);
+        polyLine.remove();
+    }
+
+    private void drawPath() {
+        if(polyLine != null) {
+            // 새로운 점이 추가되면 이전 path를 다 지우고 다시 시작점에서 그리기 위한 코드
+            polyLine.remove();
+        }
+        polyLine.setPoints(listLocation);
+        LatLng[] paths = new LatLng[listLocation.size()];
+        PolylineOptions polylineOptions = new PolylineOptions()
+                .add(listLocation.toArray(paths))
+                .width(12)
+                .color(Color.RED);
+        polyLine = googleMap.addPolyline(polylineOptions);
+    }
         });
     }
 }
--- IoTWebProgramming/src/main/java/com/mycompany/myapp/controller/HttpController.java
+++ IoTWebProgramming/src/main/java/com/mycompany/myapp/controller/HttpController.java
@@ -2,8 +2,14 @@ package com.mycompany.myapp.controller;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Date;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +19,8 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller	
 @RequestMapping("/http")
 public class HttpController {
+	private static final Logger LOGGER = LoggerFactory.getLogger(HttpController.class);
+	
 	@RequestMapping("/exam01")
 	public String exam01(
 			@RequestParam(defaultValue="0")double thermistor, @RequestParam(defaultValue="0")double photoResistor, Model model) {
@@ -31,12 +39,54 @@ public class HttpController {
 		// 중복이미지파일 저장을 막기위해 시간정보를 넣어줌.
 		String savedfilename = new Date().getTime() + "-" + attach.getOriginalFilename();
 		String savedfilepath = "C:/Temp/" + savedfilename;
-		attach.transferTo(new File( savedfilepath ));
+		attach.transferTo(new File( savedfilepath ));	
 		model.addAttribute("title", title);
 		model.addAttribute("content", content);
 		model.addAttribute("originalfilename", attach.getOriginalFilename());
 		model.addAttribute("filecontent", attach.getContentType());
 		model.addAttribute("savedfilename", savedfilename);
 		return "http/exam02";
+	}
+	
+	@RequestMapping("/exam03")
+	public void exam03(String param1, String param2, HttpServletResponse response) throws IOException {
+		LOGGER.info(param1);
+		LOGGER.info(param2);
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("param1", param1);
+		jsonObject.put("param2", param2);
+		String json = jsonObject.toString();
+		
+		response.setContentType("application/json; charset=UTF-8");
+		PrintWriter pw = response.getWriter();
+		pw.write(json);
+		pw.flush();
+		pw.close();
+	}
+	
+	@RequestMapping("/exam04")
+	public void exam04(String param1, String param2, MultipartFile attach, HttpServletResponse response) throws IOException {
+		LOGGER.info(param1);
+		LOGGER.info(param2);
+		LOGGER.info(attach.getOriginalFilename());
+		LOGGER.info(attach.getContentType());
+		LOGGER.info(String.valueOf(attach.getSize()));
+		
+		String fileName = new Date().getTime() + attach.getOriginalFilename();
+		attach.transferTo(new File("C:/Temp/" + fileName));
+		
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("param1", param1);
+		jsonObject.put("param2", param2);
+		jsonObject.put("fileName", attach.getOriginalFilename());
+		jsonObject.put("fileSize", attach.getSize());
+		jsonObject.put("fileType", attach.getContentType());
+		String json = jsonObject.toString();
+		
+		response.setContentType("application/json; charset=UTF-8");
+		PrintWriter pw = response.getWriter();
+		pw.write(json);
+		pw.flush();
+		pw.close();
 	}
 }
